4591331ac6655d997ecfaa85c186862bfa6832fc
cultpenguin <cultpenguin@949ff505-4645-4bb6-9d4a-2c039b90b574>
2015-06-24 15:31:55 +0000

    Fast writing of SEGY files when trace headers neet not be accessible

---
+++ trunk/WriteSegyFast.m
@@ -0,0 +1,40 @@
+% WriteSegyFast: Fast writing of SegyFile 
+%
+% Example = 
+%
+%
+%   [Data,SH,STH]=ReadSegyFast('file1.sgy');
+%   % Change work on Data, the write the segy using the exact same SegyHeader 
+%   % and SegyTraceHeader
+%   
+%   WriteSegyFast('file2.segy',D,SH,STH);
+%
+% Not: This will only work using the output from ReadSegyFast, which 
+% does not read to actual TraceHeader information in a readable form (only
+% as a binary BLOB)
+%
+% See also: ReadSegyFast
+% 
+function WriteSegyFast(filename,Data,SegyHeader,SegyTraceHeadersBinary)
+
+segyid = fopen(filename,'w','b');   % ALL DISK FILES ARE IN BIG
+                                    % ENDIAN FORMAT, ACCORDING SEG
+                                    % Y rev 1
+                                    
+                                    
+           
+% Write SEGY HEADER
+SegyHeader=PutSegyHeader(segyid,SegyHeader);
+
+% convert IBM formatted data?
+if SegyHeader.DataSampleFormat==1
+end
+if (strcmp(SegyHeader.DataFormat,'uint32')==1), % IBM FLOATING POINT
+        % CONVERT FROM FLOATING POINT
+        verbose_level=2;
+        SegymatVerbose([mfilename,'Converting to IBM, DataFormat :',SegyHeader.DataFormat],verbose_level)
+        Data=num2ibm(Data);
+end;
+
+% Write Segy Data
+fwrite(segyid,[SegyTraceHeadersBinary;Data],SegyHeader.DataFormat);
